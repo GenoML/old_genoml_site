2505eb2656ea0f0dadd4b628cbd04b403f8f8417
Faraz Faghri <faraz.faghri@gmail.com>
2019-03-14 15:09:01 -0400

    help

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,7 +40,7 @@ const siteConfig = {
   headerLinks: [
     {doc: 'about', label: 'About GenoML'},
     {doc: 'quick-start', label: 'Getting Started'},
-    {page: 'doc', label: 'Help'},
+    {page: 'help', label: 'Help'},
     {href: 'https://github.com/GenoML', label: 'GitHub' },
     // {blog: true, label: 'Blog'},
   ],
