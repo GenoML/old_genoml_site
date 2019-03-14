35634d89b1795fa083a25aa20cefe2523729b545
Faraz Faghri <faraz.faghri@gmail.com>
2019-03-14 12:15:33 -0400

    intro

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,9 +38,8 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'overview', label: 'Getting Started'},
-    {doc: 'example1', label: 'Examples'},
-    {doc: 'community', label: 'Community' },
+    {doc: 'about', label: 'About GenoML'},
+    {doc: 'quick-start', label: 'Getting Started'},
     {page: 'help', label: 'Help'},
     {href: 'https://github.com/GenoML', label: 'GitHub' },
     // {blog: true, label: 'Blog'},
